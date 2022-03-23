058bfd783717ec84eb9e896c0d511b7177e5b77e
Ygor-Matos <ygorsilvamatos@hotmail.com>
2022-03-23 18:12:20 -0300

    adicionada a classe placa

---
+++ OrcamentoPlacasSolares/src/util/placa.java
@@ -0,0 +1,13 @@
+package util;
+
+public class placa {
+	private String modeloPlaca;
+
+	public String getModeloPlaca() {
+		return modeloPlaca;
+	}
+
+	public void setModeloPlaca(String modeloPlaca) {
+		this.modeloPlaca = modeloPlaca;
+	}
+}
