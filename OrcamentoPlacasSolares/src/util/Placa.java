1e03844b60baf023eca7758d6e69cfcc1b07fb56
Guilherme Rodrigues <guilherme.ads.2022@gmail.com>
2022-03-23 21:51:37 -0300

    mudando nome de classes

--- OrcamentoPlacasSolares/src/util/Placa.java
+++ OrcamentoPlacasSolares/src/util/Placa.java
@@ -2,12 +2,13 @@ package util;
 
 public class Placa {
 	private String modeloPlaca;
+	private float potenciaPlaca;
 
-	public String getModeloPlaca() {
-		return modeloPlaca;
-	}
+	public String getModeloPlaca() { return modeloPlaca; }
 
-	public void setModeloPlaca(String modeloPlaca) {
-		this.modeloPlaca = modeloPlaca;
-	}
+	public void setModeloPlaca(String modeloPlaca) { this.modeloPlaca = modeloPlaca; }
+
+	public float getPotenciaPlaca() { return potenciaPlaca; }
+
+	public void setPotenciaPlaca(float potenciaPlaca) { this.potenciaPlaca = potenciaPlaca; }
 }
